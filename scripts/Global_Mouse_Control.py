97f33e10a89c87b28542db72e877eb04c62c32bf
Eric Liu <liueric101@gmail.com>
2017-02-22 17:31:55 -0800

    latest

--- scripts/Global_Mouse_Control.py
+++ scripts/Global_Mouse_Control.py
@@ -1,18 +1,13 @@
-
+import time
 def onUnlock():
 	myo.rotSetCenter()
 	myo.unlock("hold")
 	
 def onPoseEdge(pose, edge):
-	if (pose == 'fist') and (edge == "on"): 
-		myo.mouse("left","click","")
-	if (pose == 'fingersSpread') and (edge == "on"):
-		myo.mouse("right","click","")
-
+    print("onPoseEdge: "+pose+", "+edge)
 
 def onPeriodic():
-	if myo.isUnlocked():
-		myo.mouseMove(600+myo.rotYaw()*2000, 500-myo.rotPitch()*2000)
-
-
+    if(myo.isUnlocked()):
+        print myo.get_active_window_title()
+        time.sleep(0.5)
 
--- scripts/Snake_Control.py
+++ scripts/Snake_Control.py
@@ -1,26 +1,22 @@
-import time
 def onUnlock():
     myo.rotSetCenter()
     myo.unlock("hold")
 	
 def onPoseEdge(pose, edge):
     if (pose == 'fist') and (edge == "on"): 
-        if myo.get_active_window_title()=="\"Vipers\"":
-            print "tru realigion"
         print myo.get_active_window_title()
     if (pose == 'fingersSpread') and (edge == "on"):
         myo.mouse("right","click","")
 
-
-def onPeriodic():
-    if (myo.isUnlocked()): #and (myo.get_active_window_title()=="\"Vipers\""):
-        print myo.getBox()
-#        if myo.getVBox() == 1:
-#            myo.keyboard("w","press","") 
-#        elif myo.getVBox() == -1:
-#            myo.keyboard("s","press","") 
-#        elif myo.getHBox() == 1:
-#            myo.keyboard("d","press","") 
-#        elif myo.getHBox() == -1:
-#            myo.keyboard("a","press","") 
-
+def onBoxChange(box, edge):
+    if (myo.isUnlocked()) and (myo.get_active_window_title()=="\"Vipers\""):
+        direction = myo.getBox()
+        print direction
+        if direction == 1:
+            myo.keyboard("w","press","") 
+        if direction == 3:
+            myo.keyboard("d","press","") 
+        if direction == 5:
+            myo.keyboard("s","press","") 
+        if direction == 7:
+            myo.keyboard("a","press","") 
